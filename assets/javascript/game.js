0ccfe4999b4049351352288d90dd163fef3db055
Trey Marak <treymarak@alfreds-mbp.attlocal.net>
2018-06-29 18:14:36 -0500

    fixing text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,138 @@
-$(document).ready()
+$(document).ready(function(){
+
+//Computer to choose a random number from 19-120
+var random = Math.floor(Math.random()*101+19);
+
+$(".computer").text(random);
+
+
+//each crystal will have a random number hidden behind it
+
+var gem1 = Math.floor(Math.random()*11+1);
+var gem2 = Math.floor(Math.random()*11+1);
+var gem3 = Math.floor(Math.random()*11+1);
+var gem4 = Math.floor(Math.random()*11+1);
+
+var userTotal = 0;
+var wins = 0;
+var losses = 0;
+
+$(".total").text("Wins: " + wins);
+$(".total").text("Losses: " + losses);
+
+function reset(){
+
+    random = Math.floor(Math.random()*101+19);
+    $(".computer").text(random);
+ 
+    gem1 = Math.floor(Math.random()*11+1);
+    gem2 = Math.floor(Math.random()*11+1);
+    gem3 = Math.floor(Math.random()*11+1);
+    gem4 = Math.floor(Math.random()*11+1);
+
+    userTotal = 0;
+
+    $(".score").text(userTotal);
+}
+
+function winner() {
+
+    $(".total").text("You Win!");
+
+    wins++;
+    $(".total").text(wins);
+   reset()
+}
+
+function loser() {
+
+    $(".total").text("You Lose!");
+
+    losses++;
+    $(".total").text(losses);
+   reset()
+}
+
+$("#one").on("click", function(){
+
+   userTotal = userTotal + gem1;
+
+   $(".score").text(userTotal);
+
+   if (userTotal === random){
+
+       winner();
+
+   }
+
+   else if (userTotal > random){
+
+       loser();
+   }
+
+   
+});
+
+$("#two").on("click", function(){
+
+    userTotal = userTotal + gem1;
+ 
+    $(".score").text(userTotal);
+ 
+    if (userTotal === random){
+ 
+        winner();
+ 
+    }
+ 
+    else if (userTotal > random){
+ 
+        loser();
+    }
+ 
+    
+ });
+
+ $("#three").on("click", function(){
+
+    userTotal = userTotal + gem1;
+ 
+    $(".score").text(userTotal);
+ 
+    if (userTotal === random){
+ 
+        winner();
+ 
+    }
+ 
+    else if (userTotal > random){
+ 
+        loser();
+    }
+ 
+    
+ });
+
+ $("#four").on("click", function(){
+
+    userTotal = userTotal + gem1;
+ 
+    $(".score").text(userTotal);
+ 
+    if (userTotal === random){
+ 
+        winner();
+ 
+    }
+ 
+    else if (userTotal > random){
+ 
+        loser();
+    }
+ 
+    
+ });
+
+
+
+});
